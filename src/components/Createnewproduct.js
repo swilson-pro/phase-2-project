a39eec4399779ca0b12de5f0d1aa9739c33fbf1a
Olivia <oliviatmanalastas@gmail.com>
2022-08-01 15:05:50 -0400

    State variables for Createnewproduct form

--- src/components/Createnewproduct.js
+++ src/components/Createnewproduct.js
@@ -1,13 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
+//  Goal is to filter out and display the company products on the page
+//  when the create button is clicked it will direct to MyCompanyProducts
+// Createnewproduct will then have input info then it will be posted on MyCompanyProducts
 
-function Createnewproduct() {
-  return ( 
-  <div>
-    <img src='empowering_makeup_logo.png' alt='your logo' />
-    <h1>create new product</h1>
+function Createnewproduct({ }) {
 
-  </div>
+  const [makeUpName, setMakeUpName] = ("")
+  const [makeUpBrand, setMakeUpBrand] = ("")
+  const [makeUpImage, setMakeUpImage] = ("")
+  const [makeUpPrice, setMakeUpPrice] = ("")
+
+  const handleMakeUpSubmit = ((e) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/products', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: makeUpName,
+        brand: makeUpBrand,
+        image: makeUpImage,
+        price: makeUpPrice
+      }) ,
+    })
+    .then((res) => res.json())
+    .then(console.log())
+  })
+
+  console.log(makeUpName)
+
+  return (
+    <div>
+      <header>
+        <img src='empowering_makeup_logo.png' alt='your logo' />
+        <h1>Create New Product</h1>
+      </header>
+      <body className="create-new-makeup-container">
+        {/* i need an input for brand, name, price, image link, and description */}
+        <form onSubmit={handleMakeUpSubmit} className="make-up-form">
+          <input type="text"
+            name="name"
+            placeholder="Product Name"
+            value={makeUpName}
+            onChange={(e) => setMakeUpName(e.target.value)}
+          />
+          <input type="text"
+            name="brand"
+            placeholder="Product Brand"
+            value={makeUpBrand}
+            onChange={(e) => setMakeUpBrand(e.target.value)}
+          />
+          <input type="text"
+            name="name"
+            placeholder="Product Image"
+            value={makeUpImage}
+            onChange={(e) => setMakeUpImage(e.target.value)}
+          />
+          <input type="text"
+            name="name"
+            placeholder="Product Price"
+            value={makeUpPrice}
+            onChange={(e) => setMakeUpPrice(parseFloat(e.target.value))}
+          />
+          <button type="submit">Create New Product</button>
+        </form>
+
+      </body>
+    </div>
   )
 }
 
--- src/components/MyCompanyProducts.js
+++ src/components/MyCompanyProducts.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React , { useEffect } from "react"
+// import MakeupCard from "./MakeupCard";
+import MakeupTable from "./MakeupTable";
 
+//  creating props to be able to get info for each product under the compnay 
+const MyCompanyProducts = ({ makeup }) => {
 
-const MyCompanyProducts = () => {
+//  Goal is to filter out and display the company products on the page
+//  when the create button is clicked it will direct to MyCompanyProducts
+// Createnewproduct will then have input info then it will be posted on MyCompanyProducts
 
     return (
       <div>
-        <h1>Insert Company Products Page</h1>
+        {/* <MakeupTable makeup={makeup} /> */}
+        <header className="my-company-header">
+          <h1>My Company Products</h1>
+          <form>
+          </form>
+        </header>
       </div>
     );
   };
